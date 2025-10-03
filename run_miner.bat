27a79bac29cde2eae0b224b0c454cdebbaee8c9c
Bryan Roberson <berobers@asu.edu>
2025-10-03 10:39:16 +0700

    Add files via upload

---
+++ run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://sg.vipor.net:5140 -u RPEXJhrKWTMLAHNyHKPwbkVSUWExwn6mGe.ROG -p x --cpu 10
+
+.\hellminer.exe -c stratum+tcp://sg.vipor.net:5040 -u RPEXJhrKWTMLAHNyHKPwbkVSUWExwn6mGe.ROG -p x --cpu 15
